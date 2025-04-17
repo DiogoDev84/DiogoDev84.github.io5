4e2f98c0519c5e56bed8c7147d927f2d0e15803c
DiogoDev84 <diogostars73@gmail.com>
2025-04-16 21:19:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
  function adicionarValor(valor) {
     display.value += valor;
 }
+function calcular() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
+    }
+}
